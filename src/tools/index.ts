b0c47947ea11ec45757f21e373fa4e899e7e966d
paxni12345-web <paxni12345@gmail.com>
2026-09-22 23:56:13 +0700

    Register disposable ephemeral tool factory

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -6,9 +6,10 @@ import { GitStatusTool, GitDiffTool, GitLogTool } from './GitTools.js';
 import { ProjectMapTool } from './ProjectMapTool.js';
 import { ProjectMemoryTool } from './ProjectMemoryTool.js';
 import { SubagentTool } from './SubagentTool.js';
+import { CreateEphemeralTool } from './EphemeralTool.js';
 
 export function createDefaultToolRegistry(): ToolRegistry {
   const registry = new ToolRegistry();
-  for (const tool of [new ListFilesTool(), new ReadFileTool(), new WriteFileTool(), new EditFileTool(), new ShellTool(), new SearchCodeTool(), new GitStatusTool(), new GitDiffTool(), new GitLogTool(), new ProjectMapTool(), new ProjectMemoryTool(), new SubagentTool()]) registry.register(tool);
+  for (const tool of [new ListFilesTool(), new ReadFileTool(), new WriteFileTool(), new EditFileTool(), new ShellTool(), new SearchCodeTool(), new GitStatusTool(), new GitDiffTool(), new GitLogTool(), new ProjectMapTool(), new ProjectMemoryTool(), new SubagentTool(), new CreateEphemeralTool(registry)]) registry.register(tool);
   return registry;
 }
